5284a41f52f7e763e8671cc3a08495979d1bea2c
zelda0079 <chan_weng_leong@outlook.com>
2024-01-30 12:59:43 +0800

    Update PYTHON_A股.py

--- PYTHON爬A股/PYTHON_A股.py
+++ PYTHON爬A股/PYTHON_A股.py
@@ -5,6 +5,7 @@ import pandas
 table1=[]
 pn=275
 count=1
+#偽裝 看第13, 14行如何找user-agent, Accept-Language非必要
 hds={'Accept-Language':'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
 
 while True:
